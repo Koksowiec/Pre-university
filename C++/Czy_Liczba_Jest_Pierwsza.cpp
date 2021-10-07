b14693e1dc9abe3ecaace192536a4be85f2a8aca
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-10-07 22:03:57 +0200

    Add files via upload

---
+++ C++/Czy_Liczba_Jest_Pierwsza.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    bool czyPierwsza = true;
+
+    cin >> n;
+
+    int* tablica;
+    tablica = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tablica[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (tablica[i] > 1)
+        {
+            for (int j = 2; j < tablica[i]; j++)
+            {
+                if (tablica[i] % j == 0)
+                {
+                    czyPierwsza = false;
+                    cout << "NIE" << endl;
+                    break;
+                }
+                else
+                {
+                    czyPierwsza = true;
+                }
+            }
+
+            if (czyPierwsza)
+            {
+                cout << "TAK" << endl;
+            }
+        }
+        else
+        {
+            cout << "NIE" << endl;
+        }
+    }
+
+    delete[] tablica;
+    return 0;
+}
