f9d0e42c74529d4dac2025b877c6ef0413b0f69b
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-10-11 18:49:06 +0200

    Add files via upload

---
+++ C++/Silnia - ile liczb, pokaz liczbe jednosci i liczbe dziesiatek.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int d = 0;
+int wynik = 1;
+string wynikSTR;
+stringstream ss;
+
+int main()
+{
+    cin >> d;
+    while(d < 1 || d>30)
+    {
+        cin >> d;
+    }
+
+    unsigned int* tablica;
+    tablica = new unsigned int [d];
+
+    for (int i = 0; i < d; i++)
+    {
+        cin >> tablica[i];
+    }
+
+    for (int i = 0; i < d; i++)
+    {
+        wynik = 1;
+        ss.str("");
+
+        if (tablica[i] == 0 || tablica[i] == 1)
+        {
+            wynik = 1;
+        }
+        else 
+        {
+            for (int j = 1; j <= tablica[i]; j++)
+            {
+                wynik = wynik * j;
+            }
+        }
+  
+        ss << wynik;
+        wynikSTR = ss.str();
+
+        for (int j = 0; j < wynikSTR.length(); j++)
+        {
+            if (wynikSTR.length() <= 1)
+            {
+                cout << "0";
+                cout << " ";
+            }
+            cout << wynikSTR[j];
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+
+    delete[] tablica;
+    
+    return 0;
+}
