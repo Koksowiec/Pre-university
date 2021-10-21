0356c5adea228a966a192b06e7109a2f715f7d27
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-10-21 20:03:24 +0200

    Add files via upload

---
+++ C++/NWD.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int a = 0, b = 0;
+int t = 0;
+
+int nwd(int a, int b)
+{
+    //niezoptymalizowany dla dużych liczb
+    /*for (int i = 1; i <= max(a, b); i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            dzielnik = i;
+        }
+    }
+
+    return dzielnik;*/
+
+    while (a != b)
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+
+    return a;
+}
+
+int main()
+{
+    cin >> t;  
+    cin.ignore();
+    
+    for (int i = 0; i < t; i++)
+    {      
+        cin >> a;
+        if (cin.peek() == ' ')
+            cin >> b;
+
+        cout << nwd(a, b) << endl;
+        
+        a = 0;
+        b = 0;
+    }
+    
+}
+
