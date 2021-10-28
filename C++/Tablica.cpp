33aa80aa379d4f95119352555999fbd0f91116fa
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-10-28 16:16:39 +0200

    Add files via upload

---
+++ C++/Tablica.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int tab[100];
+    int i = 0;
+
+    do 
+    {
+        cin >> tab[i];
+        i++;
+    } 
+    while (cin.get() != '\n');
+
+
+    for (int j = i-1; j >=0; j--)
+    {
+        cout<<tab[j]<<" ";
+    }
+    
+
+
+    return 0;
+}
