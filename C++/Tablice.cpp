8b8bd22348d6c812d932a6077ed94fe110702dbf
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-10-28 13:31:03 +0200

    Add files via upload

---
+++ C++/Tablice.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t,n, *tab;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        tab = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> tab[j];
+        }
+
+        for (int j = (n-1); j >= 0; j--)
+        {
+            cout << tab[j] << " ";
+        }
+
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
