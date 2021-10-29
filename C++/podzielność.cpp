e3927bf0f222e1478a9a6fe780e834535db66737
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-10-29 15:21:20 +0200

    Add files via upload

---
+++ C++/podzielność.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main()
+{
+	int t, n, tab[2];
+
+	cin >> t;
+	for (int a = 0; a < t; a++)
+	{
+		cin >> n;
+		for (int i = 0; i < 2; i++)
+		{
+			cin >> tab[i];
+		}
+
+		for (int i = 1; i < n; i++)
+		{
+			if ((i % tab[0] == 0) && (i % tab[1] != 0))
+			{
+				cout << i << " ";
+			}
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
