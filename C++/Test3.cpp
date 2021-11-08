28b97113a1347e456d7e273ce23d245cf9b25f21
Koksowiec <44843822+Koksowiec@users.noreply.github.com>
2021-11-08 19:31:56 +0100

    Add files via upload

---
+++ C++/Test3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int liczba = 0, pomocnik = 0, licznik = 0;
+
+    while (cin>>liczba)
+    {
+        cout << liczba << endl;
+        if (liczba == 42 && pomocnik == 1)
+        {
+            licznik++;
+        }
+
+        if (liczba != 42)
+        {
+            pomocnik = 1;
+        }
+        else {
+            pomocnik = 0;
+        }
+
+        if (licznik == 3)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
